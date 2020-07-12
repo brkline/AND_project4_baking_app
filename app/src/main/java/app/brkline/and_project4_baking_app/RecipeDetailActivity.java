95ce371bf1bb2a0b17a31d90ffda751d3bebe80a
brkline <brkline@users.noreply.github.com>
2020-07-12 10:33:47 -0400

    Completed ingredients fragment but UI is not polished.

--- app/src/main/java/app/brkline/and_project4_baking_app/RecipeDetailActivity.java
+++ app/src/main/java/app/brkline/and_project4_baking_app/RecipeDetailActivity.java
@@ -1,11 +1,15 @@
 package app.brkline.and_project4_baking_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
+import app.brkline.and_project4_baking_app.adapters.RecipeIngredientsAdapter;
 import app.brkline.and_project4_baking_app.models.Recipe;
 
 public class RecipeDetailActivity extends AppCompatActivity {
@@ -33,6 +37,12 @@ public class RecipeDetailActivity extends AppCompatActivity {
         }
 
         setTitle(recipe.getName());
+        RecipeIngredientsAdapter recipeIngredientsAdapter = new RecipeIngredientsAdapter(RecipeDetailActivity.this, recipe.getIngredients());
+        RecyclerView.LayoutManager recipeIngredientsLayoutManager = new LinearLayoutManager(RecipeDetailActivity.this);
+        RecyclerView recipeIngredientsRecyclerView = findViewById(R.id.fragment_ingredient_rv);
+        recipeIngredientsRecyclerView.setVisibility(View.VISIBLE);
+        recipeIngredientsRecyclerView.setLayoutManager(recipeIngredientsLayoutManager);
+        recipeIngredientsRecyclerView.setAdapter(recipeIngredientsAdapter);
     }
 
     private void closeOnError() {
--- app/src/main/java/app/brkline/and_project4_baking_app/adapters/RecipeIngredientsAdapter.java
+++ app/src/main/java/app/brkline/and_project4_baking_app/adapters/RecipeIngredientsAdapter.java
@@ -2,20 +2,24 @@ package app.brkline.and_project4_baking_app.adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-import app.brkline.and_project4_baking_app.databinding.RecipeListItemBinding;
+import app.brkline.and_project4_baking_app.R;
+import app.brkline.and_project4_baking_app.databinding.RecipeIngredientListItemBinding;
 import app.brkline.and_project4_baking_app.models.Ingredient;
 
-public class RecipeIngredientsAdapter extends RecyclerView.Adapter<IngredientsViewHolder> {
+public class RecipeIngredientsAdapter extends RecyclerView.Adapter<RecipeIngredientsAdapter.IngredientsViewHolder> {
 
     private final Context context;
     private final List<Ingredient> ingredients;
+//    RecipeIngredientListItemBinding ingredientListItemBinding;
 
     public RecipeIngredientsAdapter(Context context, List<Ingredient> ingredients) {
         this.context = context;
@@ -25,18 +29,46 @@ public class RecipeIngredientsAdapter extends RecyclerView.Adapter<IngredientsVi
     @NonNull
     @Override
     public IngredientsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        RecipeListItemBinding recipeListItemBinding = RecipeListItemBinding.inflate(layoutInflater, parent, false);
-        return new RecipeAdapter.RecipeViewHolder(recipeListItemBinding);
+//        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+//        ingredientListItemBinding = RecipeIngredientListItemBinding.inflate(layoutInflater, parent, false);
+//        return new IngredientsViewHolder(ingredientListItemBinding);
+        View view = LayoutInflater.from(context).inflate(R.layout.recipe_ingredient_list_item, parent, false);
+        return new IngredientsViewHolder(view);
     }
+
+    class IngredientsViewHolder extends RecyclerView.ViewHolder {
+
+        TextView ingredientTextView;
+        TextView quantityTextView;
+        TextView measureTextView;
+
+        public IngredientsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ingredientTextView = itemView.findViewById(R.id.recipe_ingredient_list_item_ingredient_tv);
+            quantityTextView = itemView.findViewById(R.id.recipe_ingredient_list_item_quantity_tv);
+            measureTextView = itemView.findViewById(R.id.recipe_ingredient_list_item_measure_tv);
+        }
+
+        public void bindIngredients(Ingredient ingredient) {
+            quantityTextView.setText(ingredient.getQuantity().toString());
+            measureTextView.setText(ingredient.getMeasure());
+            ingredientTextView.setText(ingredient.getIngredient());
+        }
+    }
+
 
     @Override
     public void onBindViewHolder(@NonNull IngredientsViewHolder holder, int position) {
-
+        holder.bindIngredients(ingredients.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if (null == ingredients) {
+            return 0;
+        } else {
+            return ingredients.size();
+        }
+
     }
 }

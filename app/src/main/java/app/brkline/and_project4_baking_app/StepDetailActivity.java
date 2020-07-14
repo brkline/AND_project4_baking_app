143ffe3ef92410b57c0e854f2932795b18db8331
brkline <brkline@users.noreply.github.com>
2020-07-14 15:34:54 -0400

    Attempted to save instance values again in RecipeDetailActivity.  It did not work.

--- app/src/main/java/app/brkline/and_project4_baking_app/StepDetailActivity.java
+++ app/src/main/java/app/brkline/and_project4_baking_app/StepDetailActivity.java
@@ -79,15 +79,15 @@ public class StepDetailActivity extends AppCompatActivity {
 //        super.onBackPressed();
 //    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == android.R.id.home) {
-            navigateUpTo(new Intent(StepDetailActivity.this, RecipeDetailActivity.class));
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//        if (id == android.R.id.home) {
+//            navigateUpTo(new Intent(StepDetailActivity.this, RecipeDetailActivity.class));
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
     private void closeOnError() {
         finish();

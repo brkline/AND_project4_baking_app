5de68798e452964dba4924180cc0637d3b0e8f9b
brkline <brkline@users.noreply.github.com>
2020-07-20 13:08:39 -0400

    Finished wiring up ExoPlayer but it is untested.

--- app/src/main/java/app/brkline/and_project4_baking_app/StepDetailFragment.java
+++ app/src/main/java/app/brkline/and_project4_baking_app/StepDetailFragment.java
@@ -2,34 +2,27 @@ package app.brkline.and_project4_baking_app;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import com.google.android.exoplayer2.DefaultLoadControl;
-import com.google.android.exoplayer2.DefaultRenderersFactory;
-import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.upstream.DataSource;
-import com.google.android.exoplayer2.SimpleExoPlayer.Builder;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
-import
-
-import javax.sql.DataSource;
 
 import app.brkline.and_project4_baking_app.databinding.FragmentStepDetailBinding;
 import app.brkline.and_project4_baking_app.databinding.ToolbarBinding;
@@ -40,7 +33,7 @@ import app.brkline.and_project4_baking_app.models.RecipeStep;
  * Use the {@link StepDetailFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class StepDetailFragment extends Fragment implements Player.EventListener  {
+public class StepDetailFragment extends Fragment implements Player.EventListener {
 
     private RecipeStep recipeStep;
     private int numberOfSteps;
@@ -48,7 +41,10 @@ public class StepDetailFragment extends Fragment implements Player.EventListener
     private FragmentStepDetailBinding fragmentStepDetailBinding;
     private ToolbarBinding toolbarBinding;
     private SimpleExoPlayer simpleExoPlayer;
-    private PlayerView playerView;
+    private boolean playWhenReady = true;
+    private int currentWindow = 0;
+    private long playbackPosition = 0;
+    private static final String TAG = StepDetailFragment.class.getName();
 //    OnStepClickListener onStepClickListener;
 
     public StepDetailFragment() {
@@ -140,12 +136,24 @@ public class StepDetailFragment extends Fragment implements Player.EventListener
         });
     }
 
+    // Exoplayer code and overrides below based on Google CodeLab located here:
+    // https://codelabs.developers.google.com/codelabs/exoplayer-intro/#0
     private void initExoPlayer() {
         if (null == simpleExoPlayer && !(recipeStep.getVideoUrl().isEmpty())) {
             fragmentStepDetailBinding.fragmentStepDetailExoplayerPv.setVisibility(View.VISIBLE);
-
-            simpleExoPlayer = new SimpleExoPlayer.Builder(getContext()).build();
+            DefaultTrackSelector trackSelector = new DefaultTrackSelector(getContext());
+            trackSelector.setParameters(
+                    trackSelector.buildUponParameters().setMaxVideoSizeSd());
+            simpleExoPlayer = new SimpleExoPlayer.Builder(getContext())
+                    .setTrackSelector(trackSelector)
+                    .build();
             fragmentStepDetailBinding.fragmentStepDetailExoplayerPv.setPlayer(simpleExoPlayer);
+            Uri uri = Uri.parse(recipeStep.getVideoUrl());
+            MediaSource mediaSource = buildMediaSource(uri);
+            simpleExoPlayer.setPlayWhenReady(playWhenReady);
+            simpleExoPlayer.seekTo(currentWindow, playbackPosition);
+            simpleExoPlayer.addListener(this);
+            simpleExoPlayer.prepare(mediaSource, false, false);
 
 //            simpleExoPlayer.addListener(this);
 //            String userAgent = Util.getUserAgent(getActivity(), getString(R.string.app_name));
@@ -158,6 +166,85 @@ public class StepDetailFragment extends Fragment implements Player.EventListener
         String userAgent = Util.getUserAgent(getContext(), getString(R.string.app_name));
         DataSource.Factory dataSourceFactory =
                 new DefaultDataSourceFactory(getContext(),userAgent);
-        return new ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
+        return new ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT >= 24) {
+            initExoPlayer();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideSystemUi();
+        if ((Util.SDK_INT < 24 || null == simpleExoPlayer)) {
+            initExoPlayer();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT < 24) {
+            releaseExoPlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT >= 24) {
+            releaseExoPlayer();
+        }
+    }
+
+    private void hideSystemUi() {
+        fragmentStepDetailBinding.fragmentStepDetailExoplayerPv.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LOW_PROFILE
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    private void releaseExoPlayer() {
+        if (null != simpleExoPlayer) {
+            playbackPosition = simpleExoPlayer.getCurrentPosition();
+            playWhenReady = simpleExoPlayer.getPlayWhenReady();
+            currentWindow = simpleExoPlayer.getCurrentWindowIndex();
+            simpleExoPlayer.removeListener(this);
+            simpleExoPlayer.release();
+            simpleExoPlayer = null;
+        }
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady,
+                                     int playbackState) {
+        String stateString;
+        switch (playbackState) {
+            case ExoPlayer.STATE_IDLE:
+                stateString = "ExoPlayer.STATE_IDLE      -";
+                break;
+            case ExoPlayer.STATE_BUFFERING:
+                stateString = "ExoPlayer.STATE_BUFFERING -";
+                break;
+            case ExoPlayer.STATE_READY:
+                stateString = "ExoPlayer.STATE_READY     -";
+                break;
+            case ExoPlayer.STATE_ENDED:
+                stateString = "ExoPlayer.STATE_ENDED     -";
+                break;
+            default:
+                stateString = "UNKNOWN_STATE             -";
+                break;
+        }
+        Log.d(TAG, "changed state to " + stateString
+                + " playWhenReady: " + playWhenReady);
     }
 }
